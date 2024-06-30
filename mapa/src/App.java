9cde7100728b483298269193b033781bf64af6f3
mahfer18 <mahfer.s@gmail.com>
2024-06-30 12:10:51 -0300

    Alterando a branch para atividade-mapa

---
+++ mapa/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Utilizando a branch main");
+        System.out.println("Utilizando a branch atividade-mapa");
+
+    }
+}
